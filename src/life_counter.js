4166ec99614a9e9e9d69b186203206f2425e532c
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2025-01-24 14:54:17 +0300

    correct test

--- src/life_counter.js
+++ src/life_counter.js
@@ -1,9 +1,13 @@
-export default function colorCounting(objectName, objectHealth){
-    if(objectHealth > 50){
-        return "healthy"
-    }else if (objectHealth> 15 && objectHealth < 51 ){
-        return "wounded"
-    }else if (objectHealth < 15){
-        return "critical"
+export default function lifeCounter(object) {
+    if (typeof object.health !== 'number' || object.health < 0) {
+        throw new Error('Invalid health value');
+    }
+
+    if (object.health > 50) {
+        return "healthy";
+    } else if (object.health >= 15) {
+        return "wounded";
+    } else {
+        return "critical";
     }
 }
