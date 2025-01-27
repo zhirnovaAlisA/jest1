691f86ca5a6648db764867551eb88e76610ab8e9
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2025-01-27 12:56:03 +0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Используем корректное объединение объектов
+      },
+    },
+  },
+  js.configs.recommended,
 ];
