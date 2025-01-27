13f1de181783ff8835b2dc60aa70271bb44dfad2
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2025-01-27 13:15:33 +0300

    .

--- eslintrc.js
+++ eslintrc.js
@@ -1,17 +1,13 @@
 module.exports = {
-    env: {
-      node: true, // Для использования 'exports'
-      jest: true, // Для использования 'test' и 'expect'
-    },
-    extends: [
-      'eslint:recommended', // Базовые рекомендации ESLint
-    ],
-    parserOptions: {
-      ecmaVersion: 2021, // Современные возможности JS
-      sourceType: 'module', // Для использования ES-модулей (import/export)
-    },
-    rules: {
-      // Ваши кастомные правила
-    },
-  };
-  
+  env: {
+    node: true, // Для Node.js
+    jest: true, // Для Jest (глобальные функции тестирования)
+  },
+  extends: ["eslint:recommended"], // Используемые правила
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+  },
+};
